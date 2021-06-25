08deb42e2426b6692485baaad9ec39295f2cabb6
dsantaca <dsantacana@everis.com>
2021-06-25 08:46:23 +0200

    first API connection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+let [responseData, setResponseData] = useState('');
+
+  useEffect(() => {
+    axios({
+      "method": "GET",
+      "url": "https://front-test-api.herokuapp.com/api/product"
+    })
+      .then((response) => {
+        setResponseData(response.data)
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      })
+
+  }, [setResponseData, responseData]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +37,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <pre>
+        <code>
+          {responseData && JSON.stringify(responseData, null, 4)}
+        </code>
+      </pre>
     </div>
   );
 }
